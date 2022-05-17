58cdb484f303c88eefbe8e85ee4f461940c81283
Ben Robson <shadowolfbusiness@gmail.com>
2022-05-17 21:43:18 +1000

    Implement Vote listener and command. (#2)
    * Implement vote casting.
    
    * Added top voter in /vote.
    
    * Moved Votifier out of Maven to local file.
    
    * Fixed up JSONPath for vote count and name.

---
+++ zander-proxy/src/main/java/com/modularenigma/zander/proxy/model/vote/VoteCast.java
@@ -0,0 +1,20 @@
+package com.modularenigma.zander.proxy.model.vote;
+
+import com.google.gson.Gson;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Builder
+public class VoteCast {
+
+    @Getter String username;
+    @Getter String voteSite;
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+
+}
