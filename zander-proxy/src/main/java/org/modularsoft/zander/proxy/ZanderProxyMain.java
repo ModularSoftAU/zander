34931359979897472f9516d1fe91c16c156fbf30
benrobson <shadowolfbusiness@gmail.com>
2023-09-25 21:18:58 +1000

    Remove out of scope commands and events.

--- zander-proxy/src/main/java/org/modularsoft/zander/proxy/ZanderProxyMain.java
+++ zander-proxy/src/main/java/org/modularsoft/zander/proxy/ZanderProxyMain.java
@@ -1,19 +1,11 @@
 package org.modularsoft.zander.proxy;
 
-import com.jayway.jsonpath.JsonPath;
-import io.github.ModularEnigma.Request;
-import io.github.ModularEnigma.Response;
 import net.md_5.bungee.api.ChatColor;
-import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 import org.modularsoft.zander.proxy.commands.*;
 import org.modularsoft.zander.proxy.events.*;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 public class ZanderProxyMain extends Plugin implements Listener {
     private static ZanderProxyMain plugin;
@@ -29,7 +21,6 @@ public class ZanderProxyMain extends Plugin implements Listener {
 
         // Command Registry
             getProxy().getPluginManager().registerCommand(this, new ping());
-            getProxy().getPluginManager().registerCommand(this, new report());
             getProxy().getPluginManager().registerCommand(this, new rules());
             getProxy().getPluginManager().registerCommand(this, new discord());
             getProxy().getPluginManager().registerCommand(this, new guides());
@@ -42,34 +33,7 @@ public class ZanderProxyMain extends Plugin implements Listener {
             getProxy().getPluginManager().registerListener(this, new UserOnLogin());
             getProxy().getPluginManager().registerListener(this, new UserOnDisconnect());
             getProxy().getPluginManager().registerListener(this, new UserOnSwitch());
-            getProxy().getPluginManager().registerListener(this, new UserOnVote());
             getProxy().getPluginManager().registerListener(this, new UserOnProxyPing());
-            getProxy().getPluginManager().registerListener(this, new UserOnPunishment());
-            getProxy().getPluginManager().registerListener(this, new UserOnPunishmentRevoke());
-
-//        ProxyServer.getInstance().getScheduler().schedule(plugin, new Runnable() {
-//            @Override
-//            public void run() {
-//                Random random = new Random();
-//
-//                // GET request to fetch tip.
-//                Request tipReq = Request.builder()
-//                        .setURL(ConfigurationManager.getConfig().get("BaseAPIURL") + "/announcement/get?announcementType=tip")
-//                        .setMethod(Request.Method.GET)
-//                        .addHeader("x-access-token", String.valueOf(ConfigurationManager.getConfig().get("APIKey")))
-//                        .build();
-//
-//                Response tipRes = tipReq.execute();
-//                String tipJson = tipRes.getBody();
-//                String link = JsonPath.read(tipJson, "$.data[0].link");
-//                String colourMessageFormat = JsonPath.read(tipJson, "$.data[0].colourMessageFormat");
-//
-//                TextComponent message = new TextComponent(ChatColor.translateAlternateColorCodes('&', "&8[&eTIP&8] " + colourMessageFormat));
-//                message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
-//                ProxyServer.getInstance().broadcast(message);
-//            }
-//        }, 2, 1, TimeUnit.MINUTES);
-
     }
 
     @Override
