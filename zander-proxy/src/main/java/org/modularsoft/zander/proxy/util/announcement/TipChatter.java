7779f9c48700be2aaab360a256ca4fead1081342
benrobson <shadowolfbusiness@gmail.com>
2023-11-12 14:15:06 +1100

    Resolve issue with TipChatter not allowing to click link.

--- zander-proxy/src/main/java/org/modularsoft/zander/proxy/util/announcement/TipChatter.java
+++ zander-proxy/src/main/java/org/modularsoft/zander/proxy/util/announcement/TipChatter.java
@@ -5,6 +5,7 @@ import io.github.ModularEnigma.Request;
 import io.github.ModularEnigma.Response;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import org.modularsoft.zander.proxy.ConfigurationManager;
@@ -32,10 +33,16 @@ public class TipChatter {
                 String json = res.getBody();
 
                 String colourMessageFormat = JsonPath.read(json, "$.data[0].colourMessageFormat");
+                String link = JsonPath.read(json, "$.data[0].link");
+
                 // Broadcast the message to all online players
                 for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
                     // Send the message to each player
                     TextComponent message = new TextComponent(ChatColor.translateAlternateColorCodes('&', String.valueOf(ConfigurationManager.getConfig().get("announcementTipPrefix"))) + ChatColor.translateAlternateColorCodes('&', colourMessageFormat));
+                    if (link != null && !link.isEmpty()) {
+                        // Set the click event only if link is true
+                        message.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, link));
+                    }
                     player.sendMessage(message);
                 }
 
