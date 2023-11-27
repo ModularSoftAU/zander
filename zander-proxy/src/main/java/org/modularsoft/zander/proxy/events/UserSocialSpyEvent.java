b26656e7f65975f95a476821cabb412ccc8f935a
benrobson <shadowolfbusiness@gmail.com>
2023-11-27 21:36:23 +1100

    Change social spy events to read /msg /tell and /w

--- zander-proxy/src/main/java/org/modularsoft/zander/proxy/events/UserSocialSpyEvent.java
+++ zander-proxy/src/main/java/org/modularsoft/zander/proxy/events/UserSocialSpyEvent.java
@@ -12,41 +12,46 @@ import org.modularsoft.zander.proxy.ZanderProxyMain;
 import org.modularsoft.zander.proxy.model.discord.spy.DiscordCommandSpy;
 import org.modularsoft.zander.proxy.model.discord.spy.DiscordSocialSpy;
 
+import java.util.Arrays;
+
 public class UserSocialSpyEvent implements Listener {
     private ZanderProxyMain plugin = ZanderProxyMain.getInstance();
 
     @EventHandler
     public void UserChatEvent(ChatEvent event) {
         ProxiedPlayer player = (ProxiedPlayer) event.getSender();
+        String command = event.getMessage().substring(1); // Remove the leading slash
 
-        String[] messageParts = event.getMessage().split(" ");
-        String targetPlayer = messageParts[1];
-        String directMessage = event.getMessage().replaceFirst("^/msg\\s+", "");
+        if (command.startsWith("msg") || command.startsWith("tell") || command.startsWith("w")) {
+            String[] messageParts = event.getMessage().split(" ");
+            String targetPlayer = messageParts[1];
+            String directMessage = String.join(" ", Arrays.copyOfRange(messageParts, 2, messageParts.length));
 
-        //
-        // Social Spy API POST
-        //
-        try {
-            DiscordSocialSpy socialSpy = DiscordSocialSpy.builder()
-                    .usernameFrom(player.getDisplayName())
-                    .usernameTo(targetPlayer)
-                    .directMessage(directMessage)
-                    .server(player.getServer().getInfo().getName())
-                    .build();
+            //
+            // Social Spy API POST
+            //
+            try {
+                DiscordSocialSpy socialSpy = DiscordSocialSpy.builder()
+                        .usernameFrom(player.getDisplayName())
+                        .usernameTo(targetPlayer)
+                        .directMessage(directMessage)
+                        .server(player.getServer().getInfo().getName())
+                        .build();
 
-            Request socialSpyReq = Request.builder()
-                    .setURL(ConfigurationManager.getConfig().get("BaseAPIURL") + "/discord/spy/directMessage")
-                    .setMethod(Request.Method.POST)
-                    .addHeader("x-access-token", String.valueOf(ConfigurationManager.getConfig().get("APIKey")))
-                    .setRequestBody(socialSpy.toString())
-                    .build();
+                Request socialSpyReq = Request.builder()
+                        .setURL(ConfigurationManager.getConfig().get("BaseAPIURL") + "/discord/spy/directMessage")
+                        .setMethod(Request.Method.POST)
+                        .addHeader("x-access-token", String.valueOf(ConfigurationManager.getConfig().get("APIKey")))
+                        .setRequestBody(socialSpy.toString())
+                        .build();
 
-            Response socialSpyRes = socialSpyReq.execute();
-            plugin.getProxy().getConsole().sendMessage(new TextComponent("Response (" + socialSpyRes.getStatusCode() + "): " + socialSpyRes.getBody()));
-        } catch (Exception e) {
-            player.disconnect(new TextComponent("An error has occurred. Is the API down?"));
-            System.out.println(e);
-            return;
+                Response socialSpyRes = socialSpyReq.execute();
+                plugin.getProxy().getConsole().sendMessage(new TextComponent("Response (" + socialSpyRes.getStatusCode() + "): " + socialSpyRes.getBody()));
+            } catch (Exception e) {
+                player.disconnect(new TextComponent("An error has occurred. Is the API down?"));
+                System.out.println(e);
+                return;
+            }
         }
     }
 }
